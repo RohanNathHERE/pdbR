5298244632aafa8bac1daf528d79baad901da2d3
Rohan Nath <rohan.nath.here@gmail.com>
2025-01-15 15:22:34 +0530

    Updated Readme

--- R/visualize_pdb.R
+++ R/visualize_pdb.R
@@ -3,28 +3,26 @@
 #' Displays a 3D interactive plot of the PDB structure.
 #' @param pdb_path Path to the PDB file.
 #' @export
-
 visualize_pdb <- function(pdb_path) {
-  library(bio3d)
 
   # Ensure file exists
   if (!file.exists(pdb_path)) {
     stop("The specified PDB file does not exist.")
   }
 
-  # Read PDB file
-  pdb <- read.pdb(pdb_path)
+  # Read PDB file using bio3d
+  pdb <- bio3d::read.pdb(pdb_path)
 
-  # 2D visualization (basic plot)
+  # 2D visualization using base plot
   plot(pdb, type = "l", col = "blue", main = "2D PDB Structure Plot")
 
-  # Interactive 3D visualization (optional enhancement)
+  # Interactive 3D visualization using r3dmol (optional)
   if (requireNamespace("r3dmol", quietly = TRUE)) {
     r3dmol::r3dmol() %>%
       r3dmol::add_model(pdb_path) %>%
       r3dmol::set_style(style = r3dmol::style_cartoon(color = "spectrum")) %>%
       r3dmol::zoom_to()
   } else {
-    message("For interactive 3D visualization, install the 'r3dmol' package.")
+    message("For interactive 3D visualization, please install the 'r3dmol' package.")
   }
 }
---
+++ README.md
@@ -0,0 +1,15 @@
+# pdbR Package
+
+The **pdbR** package provides tools for working with PDB (Protein Data Bank) files in R, allowing for easy retrieval and visualization of protein structures. This package supports both basic 2D plotting and interactive 3D visualization of PDB structures.
+
+## Installation
+
+You can install the latest version of `pdbR` from GitHub using the `devtools` package.
+
+```r
+# Install devtools if not already installed
+install.packages("devtools")
+
+# Install pdbR from Github
+devtools::install_github("RohanNathHERE/pdbR")
+```
